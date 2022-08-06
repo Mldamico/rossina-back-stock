623aa51fa39915ce645ec976117cd81121857ffc
mldamico <matias.lioneldamico@gmail.com>
2022-08-06 14:01:42 -0300

    DB config and product basic folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: 'postgres',
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    ConfigModule.forRoot(),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
